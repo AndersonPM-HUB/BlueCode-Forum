113bcef59c53542b40ca9b7406e09443b369e78d
EdwinB5 <edwinbecerra2001@gmail.com>
2022-10-30 03:59:16 -0500

    Working on sessions

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,10 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import cookieParser from 'cookie-parser';
+
 import hilosRoutes from './routes/hilos.routes.js';
 
 dotenv.config({ path: './.env.development.local', enconding: 'latin1' });
@@ -14,12 +18,46 @@ const port = process.env.PORT;
 //Middlewares
 app.use(express.json());
 app.use(cors());
+app.use(session({
+	secret: process.env.SECRET_KEY,
+	name: process.env.SESSION_NAME,
+	cookie: {
+		httpOnly: true,
+		secure: true,
+		sameSite: false,
+	},
+	saveUninitialized: false,
+	resave: true,
+	store: MongoStore.create({
+		mongoUrl: process.env.DB_URI,
+		touchAfter: 24 * 3600,
+		ttl: 14 * 24 * 60 * 60,
+		autoRemove: 'native'
+	}),
+}));
+app.use(cookieParser());
 
 // Rutas de la aplicación
 app.use('/hilos', hilosRoutes);
 
+// test session
+app.get('/', async (req, res) => {
+	if(req.session.page_views){
+		req.session.page_views++;
+	} else {
+		req.session.page_views = 1;
+	}
+
+	//TODO Regenerar session despues de un login
+	//TODO Destruir session cuando se salga de la session
+	console.log(req.cookies)
+	console.log('Signed Cookies: ', req.signedCookies)
+	//console.log(req.sessionID);
+	res.json(req.session);
+})
+
 // Inicio de la aplicación
 app.listen(port, () => {
 	console.log(`Listen on: ${port}`);
-	console.log(`App on localhost:${port}`);
+	console.log(`App on http://localhost:${port}`);
 });
